3e1f6e2a6b8726507cef2e00518aaa2a5162544b
yoyo930021 <yoyo930021@gmail.com>
2019-10-28 22:31:15 +0800

    test: add ts config test

--- tests/testTSFile.spec.ts
+++ tests/testTSFile.spec.ts
@@ -1,6 +1,10 @@
 import { convertFile } from '../src'
 import { FileKind } from '../src/file'
 import path from 'path'
+import util from 'util'
+import { exec } from 'child_process'
+
+const execAsync = util.promisify(exec)
 
 describe('testTSFile', () => {
   const filePath = 'fixture/Input.ts'
@@ -25,13 +29,12 @@ describe('testTSFile', () => {
     expect(result).toMatchSnapshot()
   })
 
-  it('compatible and ts config file', () => {
-    const { file, result } = convertFile(filePath, __dirname, 'config/.compatible.vc2c.ts')
-    expect(file.fsPath.includes(path.basename(filePath))).toBeTruthy()
-    expect(path.isAbsolute(file.fsPath)).toBeTruthy()
-    expect(file.kind).toBe(FileKind.TS)
-    expect(file).not.toHaveProperty('start')
-    expect(file).not.toHaveProperty('end')
-    expect(result).toMatchSnapshot()
+  it('compatible and ts config file', async (done) => {
+    const { stdout, stderr } = await execAsync(`node ${path.resolve(__dirname, '../bin/vc2c')} single -v -r ${__dirname} -c config/.compatible.vc2c.ts ${filePath}`)
+
+    expect(stdout).toMatchSnapshot('stdout')
+    expect(stderr).toMatchSnapshot('stderr')
+
+    done()
   })
 })
