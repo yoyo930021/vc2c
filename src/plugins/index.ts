d22c26bb29a8741a6068be3067f6ad8539b7d315
yoyo930021 <yoyo930021@gmail.com>
2019-11-03 22:17:34 +0800

    fix: error handling can't convert object property

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -105,7 +105,7 @@ export function getASTResults (
               }
             }
             if (!converted) {
-              astResults.push(convertNodeToASTResult(tsModule, node))
+              astResults.push(convertNodeToASTResult(tsModule, property))
             }
           }
         })
