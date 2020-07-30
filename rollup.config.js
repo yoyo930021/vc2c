cd8cd7a1e1331c7b27cb2ffa77151fd2bf167bb6
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-30 23:12:45 +0800

    add rollup and referenced dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,46 @@
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
 import typescript from 'rollup-plugin-typescript2'
 
-const pluginTypeScript = typescript({ useTsconfigDeclarationDir: true })
+const pluginTypeScript = typescript({
+  useTsconfigDeclarationDir: true,
+  tsconfig: 'tsconfig.compile.json'
+})
 const pluginJson = json()
-const pluginResolve = nodeResolve()
 const pluginCommonJs = commonjs()
 
 export default [
   {
     input: 'src/index.ts',
-    output: { file: 'dist/bundle.cjs.js', format: 'cjs' },
-    plugins: [pluginJson, pluginResolve, pluginCommonJs, pluginTypeScript]
-  },
-  {
-    input: 'src/index.ts',
-    output: { file: 'dist/bundle.esm.js', format: 'es' },
-    plugins: [pluginJson, pluginResolve, pluginTypeScript]
-  },
-  {
-    input: 'src/index.ts',
-    output: { file: 'dist/index.cjs.js', format: 'cjs' },
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs'
+    },
     plugins: [pluginJson, pluginCommonJs, pluginTypeScript]
   },
   {
     input: 'src/index.ts',
-    output: { file: 'dist/index.esm.js', format: 'es' },
+    output: {
+      file: 'dist/index.mjs',
+      format: 'es'
+    },
     plugins: [pluginJson, pluginTypeScript]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.browser.mjs',
+      format: 'es'
+    },
+    plugins: [replace({ 'process.env.BROWSER': 'true' }), pluginJson, pluginTypeScript]
+  },
+  {
+    input: 'src/cli.ts',
+    output: {
+      file: 'dist/cli.js',
+      format: 'cjs'
+    },
+    plugins: [pluginJson, pluginCommonJs, pluginTypeScript]
   }
 ]
