e972822fc5549227c160fed612c660110b6f47e0
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-30 23:15:55 +0800

    fix interface trait selection

--- src/file.ts
+++ src/file.ts
@@ -1,65 +1,78 @@
-import path from 'path'
-import fs from 'fs'
-import { isVueFile, parseVueFile } from './utils'
-import { Vc2cOptions } from './options'
 import { log } from './debug'
+import { Vc2cOptions } from './options'
+import { isVueFile, parseVueFile } from './utils'
+import { isAbsolute, resolve } from 'path'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 
 export enum FileKind {
   VUE,
   TS
 }
 
-type FileInfo = {
-  fsPath: string,
-  kind: FileKind,
-  content: string
-} | {
-  fsPath: string,
-  kind: FileKind,
-  fileContent: string,
-  start: number,
-  end: number,
+export type VueFileInfo = FileInfo
+
+export interface VueSFCFileInfo extends FileInfo {
+  fileContent: string
+  start: number
+  end: number
+}
+
+export interface FileInfo {
+  fsPath: string
+  kind: FileKind
   content: string
 }
 
 export function readVueSFCOrTsFile (filePath: string, options: Vc2cOptions): FileInfo {
-  const fileFsPath = (path.isAbsolute(filePath)) ? filePath : path.resolve(options.root, filePath)
-  const fileContent = fs.readFileSync(fileFsPath, { encoding: 'utf8' })
+  if (process.env.BROWSER) {
+    throw new Error('unsupported')
+  }
+  const fileFsPath: string = (isAbsolute(filePath)) ? filePath : resolve(options.root, filePath)
+  const fileContent = readFileSync(fileFsPath, { encoding: 'utf8' })
   if (isVueFile(fileFsPath)) {
     const scriptContent = parseVueFile(options.vueTemplateCompiler, fileContent).script
-    if (scriptContent) {
-      log(`Readed Vue file: ${fileFsPath}`)
+    if (scriptContent?.content != null) {
+      log(`Read Vue file: ${fileFsPath}`)
       return {
         fsPath: fileFsPath,
         kind: FileKind.VUE,
-        start: scriptContent.start,
-        end: scriptContent.end,
-        content: scriptContent.content,
+        start: scriptContent?.start ?? 0,
+        end: scriptContent?.end ?? 0,
+        content: scriptContent?.content ?? '',
         fileContent
-      }
+      } as VueSFCFileInfo
     }
-    throw new Error('The Vue SFC don\'t have sciprt element.')
+    throw new Error('The Vue SFC don\'t have script element.')
   } else {
-    log(`Readed TS file: ${fileFsPath}`)
+    log(`Read TS file: ${fileFsPath}`)
     return {
       fsPath: fileFsPath,
       kind: FileKind.TS,
       content: fileContent
-    }
+    } as VueFileInfo
   }
 }
 
-export function writeFileInfo (fileInfo: FileInfo, content: string) {
+export function writeFileInfo (fileInfo: VueFileInfo | VueSFCFileInfo, content: string) {
+  if (process.env.BROWSER) {
+    throw new Error('unsupported')
+  }
+
+  const { fsPath } = fileInfo
   if ('start' in fileInfo) {
-    log(`Write Vue file: ${fileInfo.fsPath}`)
-    const fileContent = `${fileInfo.fileContent.slice(0, fileInfo.start)}\n${content}${fileInfo.fileContent.slice(fileInfo.end)}`
-    fs.writeFileSync(fileInfo.fsPath, fileContent, { encoding: 'utf8' })
+    const { fileContent, start, end } = fileInfo as unknown as VueSFCFileInfo
+    log(`Write Vue file: ${fsPath}`)
+    const newFileContent = `${fileContent.slice(0, start)}\n${content}${fileContent.slice(end)}`
+    writeFileSync(fsPath, newFileContent, { encoding: 'utf8' })
   } else {
-    log(`Write TS file: ${fileInfo.fsPath}`)
-    fs.writeFileSync(fileInfo.fsPath, content, { encoding: 'utf8' })
+    log(`Write TS file: ${fsPath}`)
+    writeFileSync(fsPath, content, { encoding: 'utf8' })
   }
 }
 
-export function existsFileSync (path: string) {
-  return fs.existsSync(path)
+export function existsFileSync (path: string): boolean {
+  if (process.env.BROWSER) {
+    throw new Error('unsupported')
+  }
+  return existsSync(path)
 }
